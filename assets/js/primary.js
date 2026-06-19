86754a29f157e1fd4d9a3942139b159c0eeed1bf
Md Adnan <md0adnan1234@gmail.com>
2026-06-19 21:20:52 +0530

    Created side nav-bar and some changes in css
    I have created side navbar and also solve  the problem of hidden whole navbar when size of the screen is below from the original size. Also i have hide the some section of the footer on mobile navigation.

---
+++ assets/js/primary.js
@@ -0,0 +1,11 @@
+function OpenSidebar() {
+    const sidebar =document.querySelector('#header-side');
+    sidebar.style.display='flex';
+}
+
+function CloseSidebar() {
+    const sidebar =document.querySelector('#header-side');
+    sidebar.style.display='none';
+}
+ 
+ 
